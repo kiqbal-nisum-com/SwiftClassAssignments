ebe79c66cd3c8ddee19b07198b366b0f6b1af072
khurram_iqbal <khurrammiqbal@ymail.com>
2017-06-24 15:25:31 +0500

    my home machine staging

--- SwiftDemo/MyPlayground.playground/Contents.swift
+++ SwiftDemo/MyPlayground.playground/Contents.swift
@@ -2,4 +2,28 @@
 
 import UIKit
 
-var str = "Hello, playground"
+enum EntityType : String {
+    case ItemType = "Item"
+    case BinType = "Bin"
+    case LocationType = "Location"
+
+}
+
+protocol EntityProtocol :class{
+
+    var entityType : EntityType? {get set}
+    func printEntityType()
+}
+
+class Item : EntityProtocol{
+    var entityType: EntityType? {get { return.ItemType} set(value){ self.entityType = value}}
+    func printEntityType(){
+    
+        print(self.entityType)
+    }
+
+}
+
+var item = Item()
+item.entityType  = EntityType(rawValue: "Bin")
+item.printEntityType()
